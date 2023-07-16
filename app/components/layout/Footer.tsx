29666b6838967150a88644a5019af334dd95ab8d
TitorPs360 <TitorPs360Gamer@gmail.com>
2023-07-17 01:42:06 +0700

    feat: Add footer

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+  return (
+    <>
+      <div className="h-[40px] w-full bg-cccDarkBlue shadow-xl flex justify-center items-center">
+        <span className="text-xl font-medium">Developed by TitorPs360</span>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
