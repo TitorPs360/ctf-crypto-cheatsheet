a24691936458479f85b2ce94e21fb392876f71f6
TitorPs360 <TitorPs360Gamer@gmail.com>
2023-07-19 01:03:37 +0700

    feat: Add credit

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -1,10 +1,23 @@
 import { FC } from 'react';
 
+import { useRouter } from 'next/navigation';
+
 const Footer: FC = () => {
+  const router = useRouter();
+
+  const redirectToGitHub = () => {
+    router.push('https://github.com/TitorPs360');
+  };
+
   return (
     <>
       <div className="h-[40px] w-full bg-cccDarkBlue shadow-xl flex justify-center items-center">
-        <span className="text-xl font-medium">Developed by TitorPs360</span>
+        <span className="text-xl font-medium">
+          Developed by{' '}
+          <span className="text-cccNeonGreen cursor-pointer" onClick={redirectToGitHub}>
+            TitorPs360
+          </span>
+        </span>
       </div>
     </>
   );
