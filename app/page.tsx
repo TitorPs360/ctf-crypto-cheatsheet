9c331c20b92ff47e018944ecac5cd0fd02705210
TitorPs360 <TitorPs360Gamer@gmail.com>
2023-07-19 00:58:21 +0700

    fix: Wrong typo

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Footer from '@components/layout/Footer';
 
 import Translator from '@components/translator/Translator';
 import Solver from '@components/solver/Solver';
-import Indentifier from '@components/indentifier/Indentifier';
+import Identifier from '@components/identifier/Identifier';
 import Language from '@components/language/Language';
 
 import {
@@ -44,8 +44,8 @@ export default function Home() {
         return <Translator />;
       case TabOption.solver:
         return <Solver />;
-      case TabOption.indentifier:
-        return <Indentifier />;
+      case TabOption.identifier:
+        return <Identifier />;
       case TabOption.language:
         return <Language />;
       default:
@@ -60,46 +60,6 @@ export default function Home() {
       {renderTab()}
 
       <Footer />
-
-      {/* <p>fromDecimal {fromDecimal('72 101 108 108 111')}</p>
-
-      <p>toDecimal {toDecimal('Hello')}</p>
-
-      <p>fromHex {fromHex('48 65 6c 6c 6f')}</p>
-
-      <p>toHex {toHex('Hello')}</p>
-
-      <p>fromBase32 {fromBase32('JBSWY3DP')}</p>
-
-      <p>toBase32 {toBase32('Hello')}</p>
-
-      <p>fromBase45 {fromBase45('%69 VDL2')}</p>
-
-      <p>toBase45 {toBase45('Hello')}</p>
-
-      <p>fromBase58 {fromBase58('9Ajdvzr')}</p>
-
-      <p>toBase58 {toBase58('Hello')}</p>
-
-      <p>fromBase62 {fromBase62('5TP3P3v')}</p>
-
-      <p>toBase62 {toBase62('Hello')}</p>
-
-      <p>fromBase64 {fromBase64('SGVsbG8=')}</p>
-
-      <p>toBase64 {toBase64('Hello')}</p>
-
-      <p>fromBase85 {fromBase85('87cURDZ')}</p>
-
-      <p>toBase85 {toBase85('Hello')}</p>
-
-      <p>fromROT13 {fromROT13('Uryyb')}</p>
-
-      <p>toROT13 {toROT13('Hello')}</p>
-
-      <p>fromROT47 {fromROT47('w6==@')}</p>
-
-      <p>toROT47 {toROT47('Hello')}</p> */}
     </div>
   );
 }
